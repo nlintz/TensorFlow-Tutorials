007292bb78f40b9625d38d5a2ee7fc3cc98e9408
Nathan Lintz <nlintz@gmail.com>
2015-11-10 16:51:05 -0500

    added logistic regression and simple net

---
+++ 3_net.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+import numpy as np
+import input_data
+
+
+def init_weights(shape):
+    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
+
+def model(X, w_h, w_o):
+    h = tf.nn.sigmoid(tf.matmul(X, w_h))
+    return tf.matmul(h, w_o)
+
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+
+X = tf.placeholder("float", [None, 784])
+Y = tf.placeholder("float", [None, 10])
+
+w_h = init_weights([784, 625])
+w_o = init_weights([625, 10])
+
+py_x = model(X, w_h, w_o)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost)
+predict_op = tf.argmax(py_x, 1)
+
+sess = tf.Session()
+init = tf.initialize_all_variables()
+sess.run(init)
+
+for i in range(100):
+    for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
+        sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
+    print i, np.mean(np.argmax(teY, axis=1) ==
+                     sess.run(predict_op, feed_dict={X: teX, Y: teY}))
