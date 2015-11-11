e862283bf3aeb5276c2a52fed78b45ed7bd024bb
Nathan Lintz <nlintz@gmail.com>
2015-11-11 12:11:43 -0500

    added in some really basic comments

--- 2_logistic_regression.py
+++ 2_logistic_regression.py
@@ -8,22 +8,22 @@ def init_weights(shape):
 
 
 def model(X, w):
-    return tf.matmul(X, w)
+    return tf.matmul(X, w) # notice we use the same model as linear regression, this is because there is a baked in cost function which performs softmax and cross entropy
 
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
 
-X = tf.placeholder("float", [None, 784])
+X = tf.placeholder("float", [None, 784]) # create symbolic variables
 Y = tf.placeholder("float", [None, 10])
 
-w = init_weights([784, 10])
+w = init_weights([784, 10]) # like in linear regression, we need a shared variable weight matrix for logistic regression
 
 py_x = model(X, w)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
-train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost)
-predict_op = tf.argmax(py_x, 1)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y)) # compute mean cross entropy (softmax is applied internally)
+train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost) # construct optimizer
+predict_op = tf.argmax(py_x, 1) # at predict time, evaluate the argmax of the logistic regression
 
 sess = tf.Session()
 init = tf.initialize_all_variables()
