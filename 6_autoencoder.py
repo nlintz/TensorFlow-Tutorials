06eed170a1745b646563299adc01cd6fa029ad65
regata <renat.g@gmail.com>
2015-12-01 12:45:15 -0500

    denoising autoencoder

--- 6_autoencoder.py
+++ 6_autoencoder.py
@@ -2,34 +2,43 @@ import tensorflow as tf
 import numpy as np
 import input_data
 
-def model(X, W, b, W_prime, b_prime):
-    Y = tf.nn.sigmoid(tf.matmul(X, W) + b)
-    Z = tf.nn.sigmoid(tf.matmul(Y, W_prime) + b_prime)
+def model(X, mask, W, b, W_prime, b_prime):
+    tilde_X = mask * X # corrupted X
+
+    Y = tf.nn.sigmoid(tf.matmul(tilde_X, W) + b) # hidden state
+    Z = tf.nn.sigmoid(tf.matmul(Y, W_prime) + b_prime) # reconstructed input
     return Z
 
+
+mnist_width = 28
+n_visible = mnist_width * mnist_width
+n_hidden = 500
+corruption_level = 0.3
+
 # create node for input data
-X = tf.placeholder("float", [None, 784])
+X = tf.placeholder("float", [None, n_visible], name='X')
+
+# create node for curruption mask
+mask = tf.placeholder("float", [None, n_visible], name='mask')
 
 # create nodes for hidden variables
-n_visible = 784
-n_hidden = 500
 W_init_max = 4 * np.sqrt(6. / (n_visible + n_hidden))
 W_init = tf.random_uniform(shape=[n_visible, n_hidden],
                            minval=-W_init_max,
                            maxval=W_init_max)
 
-W = tf.Variable(W_init, name="W")
-b = tf.Variable(tf.zeros([n_hidden]))
+W = tf.Variable(W_init, name='W')
+b = tf.Variable(tf.zeros([n_hidden]), name='b')
 
-W_prime = tf.transpose(W)
+W_prime = tf.transpose(W) # tight weights between encoder and decoder
 b_prime = tf.Variable(tf.zeros([n_visible]))
 
 # build model graph
-Z = model(X, W, b, W_prime, b_prime)
+Z = model(X, mask, W, b, W_prime, b_prime)
 
 # create cost function
-cost = tf.reduce_sum(tf.pow(X - Z, 2))
-train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost) # construct an optimizer
+cost = tf.reduce_sum(tf.pow(X - Z, 2)) # minimize squared error
+train_op = tf.train.GradientDescentOptimizer(0.02).minimize(cost) # construct an optimizer
 
 # load MNIST data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -39,7 +48,12 @@ sess = tf.Session()
 init = tf.initialize_all_variables()
 sess.run(init)
 
-for i in range(100):
-    for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
-        sess.run(train_op, feed_dict={X: trX[start:end]})
-    print i, sess.run(cost, feed_dict={X: teX})
+for i in range(20):
+    for start, end in zip(range(0, len(trX), 100), range(100, len(trX), 100)):
+        input_ = trX[start:end]
+        mask_np = np.random.binomial(1, 1 - corruption_level, input_.shape)
+        sess.run(train_op, feed_dict={X: input_, mask: mask_np})
+
+    mask_np = np.random.binomial(1, 1 - corruption_level, teX.shape)
+    print i, sess.run(cost, feed_dict={X: teX, mask: mask_np})
+
