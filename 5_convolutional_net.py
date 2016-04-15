b60da7a10f3dbc9c7f7c10efe703263dff5f300c
hunkim <hunkim@gmail.com>
2016-04-15 09:23:57 +0800

    Added conv numbers and shapes after each operation for easy understanding

--- 5_convolutional_net.py
+++ 5_convolutional_net.py
@@ -10,23 +10,23 @@ def init_weights(shape):
 
 
 def model(X, w, w2, w3, w4, w_o, p_keep_conv, p_keep_hidden):
-    l1a = tf.nn.relu(tf.nn.conv2d(X, w,
+    l1a = tf.nn.relu(tf.nn.conv2d(X, w,                       # l1a shape=(?, 28, 28, 32)
                         strides=[1, 1, 1, 1], padding='SAME'))
-    l1 = tf.nn.max_pool(l1a, ksize=[1, 2, 2, 1],
+    l1 = tf.nn.max_pool(l1a, ksize=[1, 2, 2, 1],              # l1 shape=(?, 14, 14, 32)
                         strides=[1, 2, 2, 1], padding='SAME')
     l1 = tf.nn.dropout(l1, p_keep_conv)
 
-    l2a = tf.nn.relu(tf.nn.conv2d(l1, w2,
+    l2a = tf.nn.relu(tf.nn.conv2d(l1, w2,                     # l2a shape=(?, 14, 14, 64)
                         strides=[1, 1, 1, 1], padding='SAME'))
-    l2 = tf.nn.max_pool(l2a, ksize=[1, 2, 2, 1],
+    l2 = tf.nn.max_pool(l2a, ksize=[1, 2, 2, 1],              # l2 shape=(?, 7, 7, 64)
                         strides=[1, 2, 2, 1], padding='SAME')
     l2 = tf.nn.dropout(l2, p_keep_conv)
 
-    l3a = tf.nn.relu(tf.nn.conv2d(l2, w3,
+    l3a = tf.nn.relu(tf.nn.conv2d(l2, w3,                     # l3a shape=(?, 7, 7, 128)
                         strides=[1, 1, 1, 1], padding='SAME'))
-    l3 = tf.nn.max_pool(l3a, ksize=[1, 2, 2, 1],
+    l3 = tf.nn.max_pool(l3a, ksize=[1, 2, 2, 1],              # l3 shape=(?, 4, 4, 128)
                         strides=[1, 2, 2, 1], padding='SAME')
-    l3 = tf.reshape(l3, [-1, w4.get_shape().as_list()[0]])
+    l3 = tf.reshape(l3, [-1, w4.get_shape().as_list()[0]])    # reshape to (?, 2048)
     l3 = tf.nn.dropout(l3, p_keep_conv)
 
     l4 = tf.nn.relu(tf.matmul(l3, w4))
@@ -37,17 +37,17 @@ def model(X, w, w2, w3, w4, w_o, p_keep_conv, p_keep_hidden):
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
-trX = trX.reshape(-1, 28, 28, 1)
-teX = teX.reshape(-1, 28, 28, 1)
+trX = trX.reshape(-1, 28, 28, 1)  # 28x28x1 input img
+teX = teX.reshape(-1, 28, 28, 1)  # 28x28x1 input img
 
 X = tf.placeholder("float", [None, 28, 28, 1])
 Y = tf.placeholder("float", [None, 10])
 
-w = init_weights([3, 3, 1, 32])
-w2 = init_weights([3, 3, 32, 64])
-w3 = init_weights([3, 3, 64, 128])
-w4 = init_weights([128 * 4 * 4, 625])
-w_o = init_weights([625, 10])
+w = init_weights([3, 3, 1, 32])       # 3x3x1 conv, 32 outputs
+w2 = init_weights([3, 3, 32, 64])     # 3x3x32 conv, 64 outputs
+w3 = init_weights([3, 3, 64, 128])    # 3x3x32 conv, 128 outputs
+w4 = init_weights([128 * 4 * 4, 625]) # FC 128 * 4 * 4 inputs, 625 outputs
+w_o = init_weights([625, 10])         # FC 625 inputs, 10 outputs (labels)
 
 p_keep_conv = tf.placeholder("float")
 p_keep_hidden = tf.placeholder("float")
