66726efb1a4f27f53f726c4ba48561a3b1d28cd4
hunkim <hunkim@gmail.com>
2016-04-15 22:53:53 +0800

    No need to have return values

--- 9_tensorboard.py
+++ 9_tensorboard.py
@@ -32,9 +32,9 @@ w_h2 = init_weights([625, 625], "w_h2")
 w_o = init_weights([625, 10], "w_o")
 
 # Add histogram summaries for weights
-w_h_simm = tf.histogram_summary("w_h_summ", w_h)
-w_h2_summ = tf.histogram_summary("w_h2_summ", w_h2)
-w_o_summ = tf.histogram_summary("w_o_summ", w_o)
+tf.histogram_summary("w_h_summ", w_h)
+tf.histogram_summary("w_h2_summ", w_h2)
+tf.histogram_summary("w_o_summ", w_o)
 
 p_keep_input = tf.placeholder("float", name="p_keep_input")
 p_keep_hidden = tf.placeholder("float", name="p_keep_hidden")
@@ -44,13 +44,13 @@ with tf.name_scope("cost") as scope:
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
     train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
     # Add scalar summary for cost
-    cost_summ = tf.scalar_summary("cost", cost)
+    tf.scalar_summary("cost", cost)
 
 with tf.name_scope("accuracy") as scope:
     correct_pred = tf.equal(tf.argmax(Y, 1), tf.argmax(py_x, 1)) # Count correct predictions
     acc_op = tf.reduce_mean(tf.cast(correct_pred, "float")) # Cast boolean to float to average
     # Add scalar summary for accuracy
-    acc_summ = tf.scalar_summary("accuracy", acc_op)
+    tf.scalar_summary("accuracy", acc_op)
 
 with tf.Session() as sess:
     # create a log writer. run 'tensorboard --logdir=./logs/nn_logs'
