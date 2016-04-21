60a835a3e975678072b8c95183daea7522213b46
hunkim <hunkim@gmail.com>
2016-04-21 09:44:33 +0800

    Removed unused variable and make it work with 0.8

--- 9_tensorboard.py
+++ 9_tensorboard.py
@@ -10,13 +10,13 @@ def init_weights(shape, name):
 # This network is the same as the previous one except with an extra hidden layer + dropout
 def model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden):
     # Add layer name scopes for better graph visualization
-    with tf.name_scope("layer1") as scope:
+    with tf.name_scope("layer1"):
         X = tf.nn.dropout(X, p_keep_input)
         h = tf.nn.relu(tf.matmul(X, w_h))
-    with tf.name_scope("layer2") as scope:
+    with tf.name_scope("layer2"):
         h = tf.nn.dropout(h, p_keep_hidden)
         h2 = tf.nn.relu(tf.matmul(h, w_h2))
-    with tf.name_scope("layer3") as scope:
+    with tf.name_scope("layer3"):
         h2 = tf.nn.dropout(h2, p_keep_hidden)
         return tf.matmul(h2, w_o)
 
@@ -39,13 +39,13 @@ p_keep_input = tf.placeholder("float", name="p_keep_input")
 p_keep_hidden = tf.placeholder("float", name="p_keep_hidden")
 py_x = model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden)
 
-with tf.name_scope("cost") as scope:
+with tf.name_scope("cost"):
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
     train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
     # Add scalar summary for cost
     tf.scalar_summary("cost", cost)
 
-with tf.name_scope("accuracy") as scope:
+with tf.name_scope("accuracy"):
     correct_pred = tf.equal(tf.argmax(Y, 1), tf.argmax(py_x, 1)) # Count correct predictions
     acc_op = tf.reduce_mean(tf.cast(correct_pred, "float")) # Cast boolean to float to average
     # Add scalar summary for accuracy
@@ -53,7 +53,7 @@ with tf.name_scope("accuracy") as scope:
 
 with tf.Session() as sess:
     # create a log writer. run 'tensorboard --logdir=./logs/nn_logs'
-    writer = tf.train.SummaryWriter("./logs/nn_logs", sess.graph_def)
+    writer = tf.train.SummaryWriter("./logs/nn_logs", sess.graph)
     merged = tf.merge_all_summaries()
 
     # you need to initialize all variables
