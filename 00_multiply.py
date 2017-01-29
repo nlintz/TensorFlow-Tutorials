a1101136a2eedb4136112574988e857e09ef77ac
ruzrobert <nufriaj@gmail.com>
2017-01-30 01:23:58 +0200

    Update 00_multiply.py to TensorFlow r1.0.0-rc0 (#72)
    * Update 00_multiply.py to TensorFlow r1.0.0-rc0
    
    From r1.0.0-rc0 there is tf.multiply , not tf.mul.
    
    * Update 00_multiply.ipynb

--- 00_multiply.py
+++ 00_multiply.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 a = tf.placeholder("float") # Create a symbolic variable 'a'
 b = tf.placeholder("float") # Create a symbolic variable 'b'
 
-y = tf.mul(a, b) # multiply the symbolic variables
+y = tf.multiply(a, b) # multiply the symbolic variables
 
 with tf.Session() as sess: # create a session to evaluate the symbolic expressions
     print("%f should equal 2.0" % sess.run(y, feed_dict={a: 1, b: 2})) # eval expressions with parameters for a and b
